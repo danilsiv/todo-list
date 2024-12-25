ab3b628beb6e2677fb730def07fe22974e3571fb
Sivkovich Danylo <danilsivkovic@gmail.com>
2024-12-25 08:18:09 +0200

    create models and format all files with black

--- planner/models.py
+++ planner/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+
+class Task(models.Model):
+    content = models.TextField()
+    datetime = models.DateTimeField(auto_now_add=True)
+    deadline = models.DateTimeField(null=True, blank=True)
+    is_done = models.BooleanField(default=False)
+    tags = models.ManyToManyField("Tag", related_name="tasks")
+
+    def __str__(self) -> str:
+        return self.content
+
+    class Meta:
+        ordering = ["is_done", "datetime"]
+
+
+class Tag(models.Model):
+    name = models.CharField(max_length=255)
