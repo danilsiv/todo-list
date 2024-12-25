693dd74a99226bbc585f8c4b75d8ba19fa11709f
Sivkovich Danylo <danilsivkovic@gmail.com>
2024-12-25 11:23:25 +0200

    create pages and views for Tag model

--- planner/urls.py
+++ planner/urls.py
@@ -6,6 +6,10 @@ from planner.views import (
     TaskUpdateView,
     TaskDeleteView,
     change_status,
+    TagListView,
+    TagCreateView,
+    TagUpdateView,
+    TagDeleteView,
 )
 
 
@@ -14,7 +18,11 @@ urlpatterns = [
     path("create/", TaskCreateView.as_view(), name="task-create"),
     path("<int:pk>/update/", TaskUpdateView.as_view(), name="task-update"),
     path("<int:pk>/delete/", TaskDeleteView.as_view(), name="task-delete"),
-    path("<int:pk>/change-status/", change_status, name="task-status-change",)
+    path("<int:pk>/change-status/", change_status, name="task-status-change"),
+    path("tags/", TagListView.as_view(), name="tag-list"),
+    path("tags/create/", TagCreateView.as_view(), name="tag-create"),
+    path("tags/<int:pk>/update/", TagUpdateView.as_view(), name="tag-update"),
+    path("tags/<int:pk>/delete/", TagDeleteView.as_view(), name="tag-delete"),
 ]
 
 app_name = "planner"
--- planner/views.py
+++ planner/views.py
@@ -8,20 +8,18 @@ from planner.models import Task, Tag
 
 class TaskListView(generic.ListView):
     model = Task
-    template_name = "planner/task_list.html"
-    context_object_name = "task_list"
 
 
 class TaskCreateView(generic.CreateView):
     model = Task
     fields = "__all__"
-    success_url = "planner:task-list"
+    success_url = reverse_lazy("planner:task-list")
 
 
 class TaskUpdateView(generic.UpdateView):
     model = Task
     fields = "__all__"
-    success_url = "planner:task-list"
+    success_url = reverse_lazy("planner:task-list")
 
 
 class TaskDeleteView(generic.DeleteView):
@@ -35,3 +33,23 @@ def change_status(request: HttpRequest, pk: int) -> HttpResponse:
     task.save()
     return redirect("planner:task-list")
 
+
+class TagListView(generic.ListView):
+    model = Tag
+
+
+class TagCreateView(generic.CreateView):
+    model = Tag
+    fields = "__all__"
+    success_url = reverse_lazy("planner:tag-list")
+
+
+class TagUpdateView(generic.UpdateView):
+    model = Tag
+    fields = "__all__"
+    success_url = reverse_lazy("planner:tag-list")
+
+
+class TagDeleteView(generic.DeleteView):
+    model = Tag
+    success_url = reverse_lazy("planner:tag-list")
